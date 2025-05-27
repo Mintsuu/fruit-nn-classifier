b7be81e45f70fa818f14d6c35ed4bec4dc6f83e6
Jared Chua <jaredchua.jr@gmail.com>
2025-05-27 19:14:39 +0800

    setup base model

---
+++ SimpleNN.py
@@ -0,0 +1,140 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from Util import load_images
+
+class SimpleCNN(nn.Module):
+  def __init__(self):
+    super(SimpleCNN, self).__init__()
+    # in_channels=1 because our image is grayscale (if color images, then in_channels=3 for RGB).
+    # out_channels=16 means we have 16 filters, each filter of size 3x3x1.
+    self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1)
+    
+    # in_channels=16 because our out_channels=16 from previous layer.
+    # out_channels=32 means we are using 32 filters, each filter of size 3x3x16,
+    # in this layer.
+    self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
+    
+    # Max Pooling Layer: downsample by a factor of 2.
+    self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+    
+    # Fully Connected Layer 1: input size = 7 * 7 * 32 (from feature maps), output size = 128.
+    self.fc1 = nn.Linear(in_features= 7 * 7 * 32, out_features=128)
+    
+    # Fully Connected Layer 2: input size = 128, output size = 10 (for 10 output classes).
+    self.fc2 = nn.Linear(in_features=128, out_features=10)
+
+    # Activation function
+    self.relu = nn.ReLU()
+
+  def forward(self, x):
+    #print(f"x.shape={x.shape}\n")
+
+    # Apply convolution + ReLU + pooling
+    x = self.conv1(x)
+    x = self.relu(x)
+    x = self.pool(x)
+
+    x = self.conv2(x)
+    x = self.relu(x)
+    x = self.pool(x)
+
+    # Flatten the feature maps 
+    x = x.view(-1, 7 * 7 * 32)
+
+    # Fully connected layers
+    x = self.fc1(x)
+    x = self.relu(x)
+    
+    # Output layer (no activation since we apply softmax in the loss function)
+    x = self.fc2(x)
+    
+    return x
+
+def train(model, criterion, optimizer, filepaths, labels):
+  # our hyper-parameters for training
+  n_epochs = 6
+  batch_size = 12 
+
+  for epoch in range(n_epochs):
+    # For tracking and printing our training-progress
+    samples_trained = 0
+    run_loss = 0
+    correct_preds = 0
+    total_samples = len(filepaths) 
+
+    permutation = torch.randperm(total_samples)
+    for i in range(0, total_samples, batch_size):
+      indices = permutation[i : i+batch_size]
+      batch_inputs = load_images(filepaths[indices])
+      batch_labels = labels[indices]
+
+      # Forward pass: coyympute predicted outputs
+      outputs = model(batch_inputs)
+
+      # Compute loss
+      loss = criterion(outputs, batch_labels)
+      run_loss += loss.item()
+
+      # Backward pass and optimization step
+      optimizer.zero_grad()
+      loss.backward()
+      optimizer.step()
+      
+      # Get probability-distributions
+      probs = torch.softmax(outputs, dim=1)
+      _, preds = torch.max(probs, dim=1)
+
+      # Calculate some stats
+      # samples_trained += len(indices)
+      samples_trained += len(batch_labels)
+      avg_loss = run_loss / samples_trained
+
+      correct_preds += torch.sum(preds == batch_labels) # compare predictions with labels
+      accuracy = correct_preds / float(samples_trained) # cast to float to get "accuracy" in decimal 
+
+      print(f"Epoch {epoch+1} " +
+            f"({samples_trained}/{total_samples}): " +
+            f"Loss={avg_loss:.5f}, Accuracy={accuracy:.5f}")
+      
+def test(model, filepaths, labels):
+  batch_size = 12
+  samples_tested = 0
+  correct_preds = 0
+  total_samples = len(filepaths)
+
+  all_preds = []
+  all_targets = []
+
+  
+  filepaths = np.array(filepaths)
+  labels = labels.clone()
+  permutation = torch.randperm(total_samples)
+  filepaths = filepaths[permutation.numpy()]
+  labels = labels[permutation]
+  
+
+
+  for i in range(0, total_samples, batch_size):
+    batch_inputs = load_images(filepaths[i : i + batch_size])
+    batch_labels = labels[i : i + batch_size]
+
+    # Forward pass: compute predicted outputs
+    outputs = model(batch_inputs)
+
+    # Get probability-distributions
+    probs = torch.softmax(outputs, dim=1)
+    _, preds = torch.max(probs, dim=1)
+
+    # Determine accuracy
+    samples_tested += len(batch_labels)
+    correct_preds += torch.sum(preds == batch_labels)
+    accuracy = correct_preds / float(samples_tested)
+
+
+    all_preds.extend(preds.cpu().numpy())
+    all_targets.extend(batch_labels.cpu().numpy())
+
+
+
+    print(f"({samples_tested}/{total_samples}): Accuracy={accuracy:.5f}")
---
+++ Util.py
@@ -0,0 +1,85 @@
+import os, torch
+import numpy as np
+from torchvision import transforms
+from PIL import Image
+
+
+def dataset_labels(labels):
+  label_list = list(set(labels))
+  print(label_list)
+
+def print_named_params(model):
+  for name, param in model.named_parameters():
+    print(f"{name}: {param.numel()}")
+
+# This is not used, since we're not storing our classified images
+# in separate folders
+
+def load_filepaths(target_dir): 
+  paths = []
+  print(target_dir)
+  files = os.listdir(target_dir)
+  for file in files:
+    paths.append(f"{target_dir}/{file}")
+  return paths
+
+
+def prepare_data(target_dir):
+  labels = []
+  # Retrieves the names of all files in the target directory
+  class_names = os.listdir(target_dir)
+  # Removing gitignore file from dataset
+  class_names.remove(".gitignore")
+  # TODO: Perform data cleansing here (e.g. renaming of misclassified dataset)
+  # Creating a dict, using the index as the key and the filename as the value
+  # class_to_idx = [name: idx for idx, name in enumerate(class_names)]
+  class_to_idx = [f"{target_dir}/{name}" for name in class_names]
+  # Substring out the name of the fruit from the filename
+  for class_name in class_names:
+    labels.append(class_name.split("_")[0])
+  # Binning each possible outcome into a dictionary
+  # 0 = banana
+  # 1 = orange
+  # 2 = apple
+  # 3 = mixed
+  binned_labels = { name: index for index, name in enumerate(sorted(set(labels))) }
+  # Converting categories into their assigned numerical bins
+  binned_outputs = [binned_labels[name] for name in labels]
+  return binned_labels, np.array(class_to_idx), torch.tensor(binned_outputs)
+
+def load_images(filepaths):
+  transform = transforms.Compose([
+    #transforms.Resize((224, 224)),
+    transforms.Resize((28, 28)),
+    transforms.ToTensor()
+    ])
+  
+
+  # Instantiate class to transform image to tensor
+  to_tensor = transforms.ToTensor()
+
+  tensor = None
+
+  # List all files in the directory
+  for item in filepaths:
+    #print("Opening:", item)
+    image = Image.open(item).convert("RGB")
+    #print(f"image size = {image.size}")
+
+    # transforms.ToTensor() performs transformations on images
+    # values of img_tensor are in the range of [0.0, 1.0]
+    img_tensor = transform(image)
+
+    #img_tensor = to_tensor(image) # convert into pytorch's tensor to work with
+    #print(f"img_tensor.shape = {img_tensor.shape}")
+    #input()
+
+    if tensor is None:
+      # size: [1,1,28,28]
+      tensor = img_tensor.unsqueeze(0) # add a new dimension
+    else:
+      # concatenate becomes [2,1,28,28], [3,1,28,28], [4,1,28,28] ...
+      # dim=0 concatenates along the axis=0 (row-wise)
+      tensor = torch.cat((tensor, img_tensor.unsqueeze(0)), dim=0)
+    
+  return tensor
